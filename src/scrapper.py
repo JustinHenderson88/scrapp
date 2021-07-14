f7d3b9783ba3da5e258130e35473e19739881c76
Justin <jus10henderson@gmail.com>
2021-07-14 07:59:02 -0500

    added for loops to parse through attributes and output to csv

--- src/scrapper.py
+++ src/scrapper.py
@@ -5,12 +5,23 @@ from selenium import webdriver
 driver = webdriver.Chrome(executable_path='/Users/justin/Documents/chromedriver')
 driver.get('https://oxylabs.io/blog')
 results = []
+other_results = []
 content = driver.page_source
 soup = BeautifulSoup(content)
+driver.quit()
 
-for element in soup.findAll(attrs='blog-card__content-wrapper'):
-    name = element.find('h2')
+#find name text in selected attritbute
+for a in soup.findAll(attrs='blog-card__content-wrapper'):
+    name = a.find('h2')
     if name not in results:
         results.append(name.text)
 
-print(results)
+#find date in selected attritbute
+for b in soup.findAll(attrs='blog-card__date-wrapper'):
+    date = b.find('h2')
+    if date not in results:
+        other_results.append(date.text)
+
+#create results csv
+df = pd.DataFrame({'Names': results, 'Dates': other_results})
+df.to_csv('names.csv', index=False, encoding='utf-8')
